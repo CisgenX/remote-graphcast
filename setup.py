2953e8672d481765ab23c7c4cd600df9ea43c544
Jonathan <jonathan@vinova.sg>
2024-12-04 17:27:41 +0800

    update dependencies version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="remote-graphcast-runpod",
-    version="0.0.15",
+    version="0.0.16",
     author="Louka Ewington-Pitsos",
     author_email="lewingtonpitsos@gmail.com",
     description="Allows people without massive GPUs to easily run graphcast on remote runpod servers",
